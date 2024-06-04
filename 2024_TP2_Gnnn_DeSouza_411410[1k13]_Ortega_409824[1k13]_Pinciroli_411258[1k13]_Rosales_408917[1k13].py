83a31017e9654e4bc45e8ae659753662d439bb55
Valentin Ortega <157056299+valentinortega@users.noreply.github.com>
2024-06-04 20:51:00 -0300

    Update 2024_TP2_Gnnn_DeSouza_411410[1k13]_Ortega_409824[1k13]_Pinciroli_411258[1k13]_Rosales_408917[1k13].py

--- 2024_TP2_Gnnn_DeSouza_411410[1k13]_Ortega_409824[1k13]_Pinciroli_411258[1k13]_Rosales_408917[1k13].py
+++ 2024_TP2_Gnnn_DeSouza_411410[1k13]_Ortega_409824[1k13]_Pinciroli_411258[1k13]_Rosales_408917[1k13].py
@@ -1,1 +1,22 @@
+# Importamos los envíos
+txt = open('envios.txt')
+envios = txt.read()
+txt.close()
 
+def tipo_de_control(envios):
+    th = ts = False
+    for car in envios:
+        if car == 'H':
+            th = True
+            ts = False
+        elif car == 'S':
+            ts = True
+            th = False
+        elif car == 'C':
+            if th:
+                return 'Hard Control'
+            elif ts:
+                return 'Soft Control'
+            th = ts = False
+
+print(' (r1) - Tipo de control de direcciones:', tipo_de_control(envios))
