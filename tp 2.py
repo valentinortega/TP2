e49be3129a08067c7feadf72fca13ecda7284ade
Rosales Nicolas <130931780+RosalesNicolas@users.noreply.github.com>
2024-06-05 00:02:24 -0300

    Add files via upload

---
+++ tp 2.py
@@ -0,0 +1,234 @@
+# Llamamos al archivo que tiene el texto
+
+archivo = open("envios25.txt", "r")
+# Asignamos "texto" a la cadena del texto completo
+with open("envios25.txt", "r") as archivo:
+    for linea in archivo:
+        pass#   print(linea)
+# texto = archivo.read()
+
+
+
+falta una funcion que saque solo el cp
+
+
+
+
+ora = ' 195256  Do Porto 1357.      32'
+
+def hc_o_sc(oracion):
+    """Esta función nos permite identificar si la estructura de control
+    es rígida (HC) o suave (SC)"""
+    h = s = False
+    control = None
+    for i in oracion:
+        if i == 'C':
+            if h:
+                control = 'Hard Control'
+            elif s:
+                control = 'Soft Control'
+        elif i == 'H':
+            h = True
+        elif i == 'S':
+            s = True
+        else:
+            h = s = False
+        if control:
+            break
+    return control
+def contar_len_cp(oracion):
+    """Esta función cuenta cuántos caracteres DIREFENTES de ' ' (blancos)
+    tiene la linea en los primeros 9 caracteres"""
+    contador = 0
+    for i in range(9):
+        if oracion[i] != ' ':
+            contador += 1
+    return contador
+# contador = contar_len_cp(ora)
+def definir_pais(oracion):
+    """Esta función nos permite ver de qué pais y región es el cp"""
+    # Usamos la función contar_len_cp() dentro de la función definir_pais
+    contador = contar_len_cp(oracion)
+    pais = None
+    if contador == 4:
+        dig = 0
+        for i in oracion[:9]:
+            if i.isdigit():
+                dig += 1
+        if dig == 4:
+            pais = 'bolivia'
+    elif contador == 5:
+        primero = True
+        mont = None
+        dig = 0
+        for i in oracion[:9]:
+            if i == ' ':
+                continue
+            elif i.isdigit():
+                dig += 1
+                if primero and i == '1':
+                    mont = True
+                else:
+                    primero = False
+        if dig == 5:
+            pais = 'uruguay'
+            if mont:
+                pais = 'montevideo'
+    elif contador == 6:
+        dig = 0
+        for i in oracion[:9]:
+            if i.isdigit():
+                dig += 1
+        if dig == 6:
+            pais = 'paraguay'
+    elif contador == 7:
+        dig = 0
+        for i in oracion[:9]:
+            if i.isdigit():
+                dig += 1
+        if dig == 7:
+            pais = 'chile'
+    elif contador == 9:
+        dig = 0
+        for i in oracion[:9]:
+            if i.isdigit():
+                dig += 1
+        if dig == 8 and oracion[5] == '-':
+            if oracion[0] == '8' or oracion[0] == '9':
+                pais = 'brasil_20'
+            elif oracion[0] == '0' or oracion[0] == '1' or oracion[0] == '2' or oracion[0] == '3':
+                pais = 'brasil_25'
+            elif oracion[0] == '4' or oracion[0] == '5' or oracion[0] == '6' or oracion[0] == '7':
+                pais = 'brasil_30'
+    elif contador == 8:
+        prov = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q',
+                'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+        verif = 0
+        primero = True
+        dig = 0
+        letra = 0
+        for i in oracion[:9]:
+            if i == ' ':
+                continue
+            elif i.isalpha() and primero:
+                primero = False
+                if i in prov:
+                    verif += 1
+                else:
+                    primero = True
+            elif i.isdigit() and dig < 4 and not primero:
+                dig += 1
+                verif += 1
+            elif i.isalpha() and dig == 4 and letra < 3:
+                letra += 1
+                verif += 1
+            else:
+                primero = True
+                verif = 0
+        if verif == 8:
+            pais = 'argentina'
+    if pais == None:
+        pais = 'otros'
+    return pais
+def precio_envio(oracion):
+    """Devuelve el id del envío y el valor a pagar. Todo en int"""
+    if oracion[-2] == '0':
+        return int(oracion[-2]), 1100
+    elif oracion[-2] == '1':
+        return int(oracion[-2]), 1800
+    elif oracion[-2] == '2':
+        return int(oracion[-2]), 2450
+    elif oracion[-2] == '3':
+        return int(oracion[-2]), 8300
+    elif oracion[-2] == '4':
+        return int(oracion[-2]), 10900
+    elif oracion[-2] == '5':
+        return int(oracion[-2]), 14300
+    elif oracion[-2] == '6':
+        return int(oracion[-2]), 17900
+def ajuste_precios(region):
+    """Devuelve el valor para multiplicar al precio para ajustar al exterior"""
+    region_20 = 'bolivia', 'paraguay', 'montevideo' 'brasil_20'
+    region_25 = 'chile', 'uruguay', 'brasil_25'
+    region_30 = 'brasil_30'
+    region_50 = 'otros'
+    if region in region_20:
+        return 1.20
+    elif region in region_25:
+        return 1.25
+    elif region in region_30:
+        return 1.30
+    elif region in region_50:
+        return 1.50
+    else:
+        return 1
+def validar_direccion(oracion):
+    cl = cn = 0
+    num = sm = False
+    solo_car = doble_mayus = True
+    for car in oracion[9:29]:
+        if 'A' <= car <= 'z':
+            cl += 1
+            if 'A' <= car <= 'Z' and sm:
+                doble_mayus = False
+            elif 'A' <= car <= 'Z':
+                sm = True
+            else:
+                sm = False
+        elif car.isdigit():
+            cn += 1
+            sm = False
+        elif car == '.' or car == ' ':
+            if cl == 0 and cn > 0:
+                num = True
+            cl = cn = 0
+            sm = False
+        else:
+            solo_car = False
+            break
+    if solo_car and num and doble_mayus:
+        return True
+    else:
+        return False
+
+direccion = validar_direccion(ora)
+print(direccion)
+
+# control = hc_o_sc(texto)
+# pais = definir_pais(ora)
+# precio = precio_envio(ora)
+# ajuste = ajuste_precios(pais)
+# print(control)
+# # print(contador)
+# print(pais)
+# print(precio)
+# print(ajuste)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# texto.close()
